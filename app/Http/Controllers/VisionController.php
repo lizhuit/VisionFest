f60f3e382d7d3c81b2dea5b0f0f42968e9c162c8
AdriTH9 <tlaxcaltecatladriana40@gmail.com>
2025-03-25 10:03:09 -0600

    Resolviendo conflictos controller

--- app/Http/Controllers/VisionController.php
+++ app/Http/Controllers/VisionController.php
@@ -20,10 +20,9 @@ class VisionController extends Controller
         public function configuracion (){
             return view('configuracion');
         }
-<<<<<<< HEAD
+
         public function correo(){
             return view('correo');
         }
-=======
->>>>>>> 0c5ef93 (Modelos de BD)
+
 }
